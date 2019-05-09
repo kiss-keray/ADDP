b108506654efdf8069b662e0cc6e87133c33fe31
keray <1172304645@qq.com>
2019-05-10 00:16:45 +0800

    有意思

--- addp-ssh/addp-ssh-web/src/main/java/com/nix/jingxun/addp/ssh/SSHStart.java
+++ addp-ssh/addp-ssh-web/src/main/java/com/nix/jingxun/addp/ssh/SSHStart.java
@@ -1,4 +1,4 @@
-package com.nix.jingxun.addp.ssh.web;
+package com.nix.jingxun.addp.ssh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
