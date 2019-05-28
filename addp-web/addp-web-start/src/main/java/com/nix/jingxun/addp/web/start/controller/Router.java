05fbe16069c8fc5a463829f97dc2bad450ffeed5
keray <1172304645@qq.com>
2019-05-28 16:59:21 +0800

    react

---
+++ addp-web/addp-web-start/src/main/java/com/nix/jingxun/addp/web/start/controller/Router.java
@@ -0,0 +1,18 @@
+package com.nix.jingxun.addp.web.start.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author keray
+ * @date 2019/05/28 16:38
+ */
+@Controller()
+@RequestMapping("/static")
+public class Router {
+    @GetMapping("/*")
+    public String react() {
+        return "/index";
+    }
+}
